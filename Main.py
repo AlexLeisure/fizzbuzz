21007b4ec03a90991e8753dd370dd8d738978d2d
Alex <alex.leisure125@gmail.com>
2020-02-29 12:34:46 -0600

    Made explanation comment more readable and fixxed grammar issues

--- Main.py
+++ Main.py
@@ -1,12 +1,8 @@
 # This program will play the game "fizzbuzz".
-# Fizzbuzz is a game where 2 players will
-# will count from 1 to some number. Every
-# time player one encounters a number that
-# is divisible by 3, they will say "fizz".
-# Every time player 2 encounters a number
-# divisible by 5, they will say "buzz".
-# When the number is divisible by both
-# 3 AND 5, they will both say "fizzbuzz"
+# Fizzbuzz is a game where 2 players will count from 1 to some number.
+# Every time player one encounters a number that is divisible by 3, they will say "fizz".
+# Every time player 2 encounters a number divisible by 5, they will say "buzz".
+# When the number is divisible by both 3 AND 5, they will both say "fizzbuzz"
 
 numCount = 100
 
